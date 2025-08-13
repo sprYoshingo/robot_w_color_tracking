bf0b1e9adfb08ecd56f3b210a5c9003dcfd275c2
sprYoshingo <zrt5062@psu.edu>
2025-08-13 11:41:11 -0400

    get direction added

--- tracking.py
+++ tracking.py
@@ -1,68 +1,94 @@
 import cv2
 import numpy as np 
 import time
+def main():
+    #open the camera
+    cam = cv2.VideoCapture(0)
 
-#open the camera
-cam = cv2.VideoCapture(0)
-
-#get the frame's height and width to find the center to determine what direction it moves
-frame_w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-center_x = frame_w//2
-center_y = frame_h//2
-
-
-#color to track (currently green)
-lower_color = np.array([35, 100, 100])
-upper_color = np.array([85, 255, 255])
-
-last_time = time.time()
-
-while True: #read from the cam until it fails / ends
-    ret,frame = cam.read()
-    if not ret:
-        break
-
-    #change frame to hsv color space
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
-
-    #seperate the tracked colors from the non tracked ones
-    mask = cv2.inRange(hsv, lower_color, upper_color)
-
-    #find the shape / outlines of the tracked colors
-    contours , ignored = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #get the frame's height and width to find the center to determine what direction it moves
+    frame_w = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_h = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
 
 
-    #find the largest shape / outline
-    if contours:
-        largest = max(contours, key=cv2.contourArea)
+    #color to track (currently green)
+    lower_color = np.array([35, 100, 100])
+    upper_color = np.array([85, 255, 255])
 
-        if cv2.contourArea(largest) > 0: #can be used to hide tiny spaces but current doesnt do that
-            x,y,w,h = cv2.boundingRect(largest)
+    last_time = time.time()
 
-            cx,cy= (x+w)//2, (y+h)//2
+    while True: #read from the cam until it fails / ends
+        ret,frame = cam.read()
+        if not ret:
+            break
+        
+        center_x = frame.shape[1]//2
+        center_y = frame.shape[0]//2
+        #change frame to hsv color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
 
-            dx = cx-center_x #distance from center
-            dy = cy-center_y
+        #seperate the tracked colors from the non tracked ones
+        mask = cv2.inRange(hsv, lower_color, upper_color)
+
+        #find the shape / outlines of the tracked colors
+        contours , ignored = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-            #draw frame for tracking
-            cv2.rectangle(frame, (x,y),(x+w,y+h), (0,255,0),2)
-            cv2.circle(frame, (cx,cy),5,(255,0,0),-1)
+        #find the largest shape / outline
+        if contours:
+            largest = max(contours, key=cv2.contourArea)
 
-            #print the time every second 
-            if time.time() - last_time > 1.0:
-                print(f"Object position: ({cx},{cy})") #change to sending to arduino later
-                print(f"Distance from center: ({dx},{dy})\n")
-                last_time = time.time()
-                
+            if cv2.contourArea(largest) > 100: #can be used to hide tiny spaces but current doesnt do that
+                x,y,w,h = cv2.boundingRect(largest)
 
-    #show the frame
-    cv2.imshow("lol", frame)
+                cx,cy= x+w//2, y+h//2
 
-    #stop tracking and close the window when 'a' is pressed
-    if cv2.waitKey(1) & 0xFF == ord('a'):
-        break
+                dx = cx-center_x #distance from center
+                dy = cy-center_y
 
-cam.release()
-cv2.destroyAllWindows()
+
+                #draw frame for tracking
+                cv2.rectangle(frame, (x,y),(x+w,y+h), (0,255,0),2)
+                #cv2.circle(frame, (cx,cy),5,(255,0,0),-1)
+
+                #print the time every second 
+                if time.time() - last_time > 1.0:
+                    print(f"Object position: ({cx},{cy})") #change to sending to arduino later
+                    print(f"Distance from center: ({dx},{dy})")
+                    print(f"Direction: {get_direction(dx,dy)}")
+                    last_time = time.time()
+                    
+
+        #show the frame
+        cv2.imshow("lol", frame)
+
+        #stop tracking and close the window when 'a' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('a'):
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+def get_direction(dx,dy):
+    direction = []
+
+    #forward if dy is pos and bacwards is dy is negative
+    if dy >0:
+        direction.append("S")
+    elif dy<0:
+        direction.append("N")
+
+    #right if dx is pos and left if dx is negative
+    if dx>0:
+        direction.append("E")
+    elif dx<0:
+        direction.append("W")
+
+    return "".join(direction) if direction else "none"
+
+    #def get_speed(dx,dy):
+
+
+
+if __name__ == "__main__":
+    main()
